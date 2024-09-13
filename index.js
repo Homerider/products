93cb4a6cc9c2524c02c8a3da7b2d32ecfb46e206
Homerider <i@ateslikov.ru>
2024-09-13 17:45:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,25 @@
 // которая вычисляет и возвращает общую стоимость запасов.
 // Умножьте цену каждого товара на его количество и сложите эти значения. Решение-1
 
-let inventory = [
-    {id: 1, name: "Apple", price: 0.50, quantity: 200},
-    {id: 2, name: "Banana", price: 0.30, quantity: 150},
-    {id: 3, name: "Cherry", price: 0.90, quantity: 50}
-]
-
-const totalInventoryValue = () => {
-    let totalIValue = 0
-    for (let i = 0; i < inventory.length; i++) {
-        let allQuantity = inventory[i].quantity
-        let allPrice = inventory[i].price
-        if (allQuantity) {
-            totalIValue += allQuantity * allPrice
-        }
-    }
-    console.log(totalIValue)
-}
-
-totalInventoryValue()
+// let inventory = [
+//     {id: 1, name: "Apple", price: 0.50, quantity: 200},
+//     {id: 2, name: "Banana", price: 0.30, quantity: 150},
+//     {id: 3, name: "Cherry", price: 0.90, quantity: 50}
+// ]
+//
+// const totalInventoryValue = () => {
+//     let totalIValue = 0
+//     for (let i = 0; i < inventory.length; i++) {
+//         let allQuantity = inventory[i].quantity
+//         let allPrice = inventory[i].price
+//         if (allQuantity) {
+//             totalIValue += allQuantity * allPrice
+//         }
+//     }
+//     console.log(totalIValue)
+// }
+//
+// totalInventoryValue()
 
 
 // Вычислить общую стоимость запасов
